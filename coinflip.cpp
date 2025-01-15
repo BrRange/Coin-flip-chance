6e2cb91d9a4ba8de4ddac9d94bda8e30b550da85
BurstRange <113287175+BrRange@users.noreply.github.com>
2025-01-15 11:48:48 -0300

    Looking at old code
    What was stdio doing there?

--- coinflip.cpp
+++ coinflip.cpp
@@ -1,47 +1,40 @@
 #include <iostream>
 #include <time.h>
-#include <stdlib.h>
 
-unsigned long long coinFlip(unsigned row);
-
-int WinMain(){
-    std::cout << "Coin flip streak game\n";
-
-    while (true){
-    srand(time(NULL));
-    double games = 0;
-    unsigned inARow = 0;
-    long double average = 0;
-    std::cout << std::endl << "What streak you desire: ";
-    std::cin >> inARow;
-    std::cout << std::endl << "In how many games: ";
-    std::cin >> games; std::cout << std::endl;
-
-    for (unsigned i = 0; i < games; i++){
-        average += coinFlip(inARow)/games;
-    }
-    std::cout << "Average of " << average << " tries in " << games << " games, to get a " << inARow << " streak! A " << 1/average*100 << "\% chance to get it!" << std::endl;
-    }
-
-    return 0;
-}
-
-unsigned long long coinFlip(unsigned row){
-    unsigned long long tries = 1;
+size_t coinFlip(unsigned row){
+    size_t tries = 1ull;
     unsigned count = 0;
     while (true){
-        unsigned rng = rand() % 2;
-        switch (rng){
-            case 0:
-                count = 0;
-                tries += 1;
-                break;
-            case 1:
+        if(rand() % 2){
+            count = 0;
+            tries += 1ull;
+        } else {
                 count += 1;
-                break;
         }
         if (count == row){
             return tries;
         }
+    }
+}
+
+int main(){
+    std::cout << "Coin flip streak game\n";
+
+    while (true){
+    srand(time(0ull));
+    double games = 0.0;
+    unsigned inARow = 0u;
+    double average = 0.0;
+    std::cout << '\n' << "What streak do you desire? ";
+    std::cin >> inARow;
+    std::cout << '\n' << "In how many games? ";
+    std::cin >> games;
+    if(!inARow || !games) return 0;
+    std::cout << '\n';
+
+    for (unsigned i = 0; i < games; i++){
+        average += coinFlip(inARow) / games;
+    }
+    std::cout << "Average of " << average << " tries in " << games << " games, to get a " << inARow << " streak! A " << 100 / average << "% chance to get it!" << '\n';
     }
 }
